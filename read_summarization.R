c387d3916fd34a178ccf171b32fe7c59ee957089
Himanshu Dashora <hrdashora@gmail.com>
2022-05-19 15:26:06 -0400

    initial commit

---
+++ read_summarization.R
@@ -0,0 +1,15 @@
+# load packages
+library(Rsubread)
+
+# input .bam files
+
+# count reads
+featureCounts(files = ,
+              
+              # annotation
+              annot.inbuilt = "hg38",)
+
+
+
+
+
