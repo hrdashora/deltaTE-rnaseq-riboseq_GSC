7db7a0487c41b3f2e586981acfb8d456a24350a7
Himanshu Dashora <hrdashora@gmail.com>
2024-02-05 18:04:05 -0500

    Rename files and calculate intersection between DTEG datasets

--- 4_dteg.R
+++ 4_dteg.R
@@ -101,7 +101,7 @@ res_hn_rna <- lfcShrink(ddsMat_hn_rna,
                         coef = "Condition_hypoxia_vs_normoxia",
                         type = "apeglm")
 write.table(res_hn_rna[which(res_hn_rna$padj < 0.05), ],
-            "results/DTGs_hn.txt",
+            "results/DEGs_hn.txt",
             quote = FALSE)
 
 ddsMat_gscnpc_rna <- DESeqDataSetFromMatrix(
@@ -115,11 +115,184 @@ res_gscnpc_rna <- lfcShrink(ddsMat_gscnpc_rna,
                             coef = "Condition_gsc_vs_npc",
                             type = "apeglm")
 write.table(res_gscnpc_rna[which(res_gscnpc_rna$padj < 0.05), ],
-            "results/DTGs_gscnpc.txt",
+            "results/DEGs_gscnpc.txt",
             quote = FALSE)
 
+# Run DESeq2 for RPFs
+ddsMat_hn_ribo <- DESeqDataSetFromMatrix(
+  countData = ribo_counts_hn,
+  colData = sample_info_hn[which(sample_info_hn$SeqType == "ribo"),],
+  design = ~ Batch + Condition
+)
+ddsMat_hn_ribo <- DESeq(ddsMat_hn_ribo)
+resultsNames(ddsMat_hn_ribo)
+res_hn_ribo <- lfcShrink(ddsMat_hn_ribo,
+                         coef = "Condition_hypoxia_vs_normoxia",
+                         type = "apeglm")
+write.table(res_hn_ribo[which(res_hn_ribo$padj < 0.05), ],
+            "results/RPFs_hn_ribo.txt",
+            quote = FALSE)
+
+ddsMat_gscnpc_ribo <- DESeqDataSetFromMatrix(
+  countData = ribo_counts_gscnpc,
+  colData = sample_info_gscnpc[which(sample_info_gscnpc$SeqType == "ribo"),],
+  design = ~ Condition
+)
+ddsMat_gscnpc_ribo <- DESeq(ddsMat_gscnpc_ribo)
+resultsNames(ddsMat_gscnpc_ribo)
+res_gscnpc_ribo <- lfcShrink(ddsMat_gscnpc_ribo,
+                             coef = "Condition_gsc_vs_npc",
+                             type = "apeglm")
+write.table(res_gscnpc_ribo[which(res_gscnpc_ribo$padj < 0.05), ],
+            "results/RPFs_gscnpc_ribo.txt",
+            quote = FALSE)
 
 # Categorize genes into distinct regulation groups ------------------------
+## Forwarded: genes driven by transcriptional regulation, change in RPF is in the same direction as change in RNA
+forwarded_gscnpc <- rownames(res_gscnpc)[which(res_gscnpc$padj > 0.05 &
+                                                 res_gscnpc_ribo$padj < 0.05 &
+                                                 res_gscnpc_rna$padj < 0.05)]
+forwarded_hn <- rownames(res_hn)[which(res_hn$padj > 0.05 &
+                                         res_hn_ribo$padj < 0.05 &
+                                         res_hn_rna$padj < 0.05)]
+## Exclusive: genes regulated exclusively by translation, change in RPF is not driven by change in RNA
+exclusive_gscnpc <- rownames(res_gscnpc)[which(res_gscnpc$padj < 0.05 & 
+                                                 res_gscnpc_ribo$padj < 0.05 & 
+                                                 res_gscnpc_rna$padj > 0.05)]
+exclusive_gscnpc_pos <- rownames(res_gscnpc)[which(res_gscnpc$log2FoldChange > 0 &
+                                                     res_gscnpc$padj < 0.05 & 
+                                                     res_gscnpc_ribo$padj < 0.05 & 
+                                                     res_gscnpc_rna$padj > 0.05)]
+exclusive_gscnpc_neg <- rownames(res_gscnpc)[which(res_gscnpc$log2FoldChange < 0 &
+                                                     res_gscnpc$padj < 0.05 & 
+                                                     res_gscnpc_ribo$padj < 0.05 & 
+                                                     res_gscnpc_rna$padj > 0.05)]
 
+exclusive_hn <- rownames(res_hn)[which(res_hn$padj < 0.05 &
+                                         res_hn_ribo$padj < 0.05 &
+                                         res_hn_rna$padj > 0.05)]
+exclusive_hn_pos <- rownames(res_hn)[which(res_hn$log2FoldChange > 0 &
+                                             res_hn$padj < 0.05 &
+                                             res_hn_ribo$padj < 0.05 &
+                                             res_hn_rna$padj > 0.05)]
+exclusive_hn_neg <- rownames(res_hn)[which(res_hn$log2FoldChange > 0 &
+                                             res_hn$padj < 0.05 &
+                                             res_hn_ribo$padj < 0.05 &
+                                             res_hn_rna$padj > 0.05)]
+## Intensified & Buffered: genes regulated by both transcriptional and translational regulation
+both_gscnpc <- rownames(res_gscnpc)[which(res_gscnpc$padj < 0.05 &
+                                            res_gscnpc_ribo$padj < 0.05 &
+                                            res_gscnpc_rna$padj < 0.05)]
+both_gscnpc_pos <- rownames(res_gscnpc)[which(res_gscnpc$log2FoldChange > 0 &
+                                                res_gscnpc$padj < 0.05 &
+                                                res_gscnpc_ribo$padj < 0.05 &
+                                                res_gscnpc_rna$padj < 0.05)]
+both_gscnpc_neg <- rownames(res_gscnpc)[which(res_gscnpc$log2FoldChange < 0 &
+                                                res_gscnpc$padj < 0.05 &
+                                                res_gscnpc_ribo$padj < 0.05 &
+                                                res_gscnpc_rna$padj < 0.05)]
+intensified_gscnpc <- both_gscnpc[which(res_gscnpc[both_gscnpc,2]*res_gscnpc_rna[both_gscnpc,2] > 0)]
+intensified_gscnpc_pos <- both_gscnpc_pos[which(res_gscnpc[both_gscnpc_pos,2]*res_gscnpc_rna[both_gscnpc_pos,2] > 0)]
+intensified_gscnpc_neg <- both_gscnpc_neg[which(res_gscnpc[both_gscnpc_neg,2]*res_gscnpc_rna[both_gscnpc_neg,2] > 0)]
 
+buffered_gscnpc <- both_gscnpc[which(res_gscnpc[both_gscnpc,2]*res_gscnpc_rna[both_gscnpc,2] < 0)]
+buffered_gscnpc <- c(rownames(res_gscnpc)[which(res_gscnpc$padj < 0.05 &
+                                                  res_gscnpc_ribo$padj > 0.05 &
+                                                  res_gscnpc_rna$padj < 0.05)],
+                     buffered_gscnpc)
+
+buffered_gscnpc_pos <- both_gscnpc_pos[which(res_gscnpc[both_gscnpc_pos,2]*res_gscnpc_rna[both_gscnpc_pos,2] < 0)]
+buffered_gscnpc_pos <- c(rownames(res_gscnpc)[which(res_gscnpc$log2FoldChange > 0 &
+                                                      res_gscnpc$padj < 0.05 &
+                                                      res_gscnpc_ribo$padj > 0.05 &
+                                                      res_gscnpc_rna$padj < 0.05)],
+                         buffered_gscnpc_pos)
+
+buffered_gscnpc_neg <- both_gscnpc_neg[which(res_gscnpc[both_gscnpc_neg,2]*res_gscnpc_rna[both_gscnpc_neg,2] < 0)]
+buffered_gscnpc_neg <- c(rownames(res_gscnpc)[which(res_gscnpc$log2FoldChange < 0 &
+                                                      res_gscnpc$padj < 0.05 &
+                                                      res_gscnpc_ribo$padj > 0.05 &
+                                                      res_gscnpc_rna$padj < 0.05)],
+                         buffered_gscnpc_neg)
+
+both_hn <- rownames(res_hn)[which(res_hn$padj < 0.05 &
+                                    res_hn_ribo$padj < 0.05 &
+                                    res_hn_rna$padj < 0.05)]
+intensified_hn <- both_hn[which(res_hn[both_hn,2]*res_hn_rna[both_hn,2] > 0)]
+buffered_hn <- both_hn[which(res_hn[both_hn,2]*res_hn_rna[both_hn,2] < 0)]
+buffered_hn <- c(rownames(res_hn)[which(res_hn$padj < 0.05 &
+                                          res_hn_ribo$padj > 0.05 &
+                                          res_hn_rna$padj < 0.05)],
+                 buffered_hn)
+
+# Visualize global translation and transcriptional regulation
+max_val <- max(res_gscnpc_ribo[,2], res_gscnpc_rna[,2], na.rm = T)
+plot(y = res_gscnpc_ribo[,2],
+     x = res_gscnpc_rna[,2],
+     xlab = "RNA-seq log2 fold change",
+     ylab = "Ribo-seq log2 fold change",
+     asp = 1, 
+     pch = 16,
+     col = rgb(128/255, 128/255, 128/255, 0.1),
+     ylim = c(-max_val, max_val),
+     xlim = c(-max_val, max_val),
+     cex = 0.4
+)
+abline(a = 0, b = 1, col = "gray")
+abline(h = 0, v = 0, col = "gray")
+points(y = res_gscnpc_ribo[forwarded_gscnpc, 2],
+       x = res_gscnpc_rna[forwarded_gscnpc, 2],
+       pch = 16,
+       col = rgb(0,0,1,1),
+       cex = 0.4)
+points(y = res_gscnpc_ribo[exclusive_gscnpc, 2],
+       x = res_gscnpc_rna[exclusive_gscnpc, 2],
+       pch = 16, 
+       col = rgb(1,0,0,1),
+       cex = 0.4)
+points(y = res_gscnpc_ribo[intensified_gscnpc, 2],
+       x = res_gscnpc_rna[intensified_gscnpc, 2],
+       pch = 16, 
+       col = rgb(1,1,0,1),
+       cex = 0.4)
+points(y = res_gscnpc_ribo[buffered_gscnpc, 2],
+       x = res_gscnpc_rna[buffered_gscnpc, 2],
+       pch = 16, 
+       col = rgb(1,0,1,1),
+       cex = 0.4)
+
+max_val <- max(res_hn_ribo[,2], res_hn_rna[,2], na.rm = T)
+plot(y = res_hn_ribo[,2],
+     x = res_hn_rna[,2],
+     xlab = "RNA-seq log2 fold change",
+     ylab = "Ribo-seq log2 fold change",
+     asp = 1, 
+     pch = 16,
+     col = rgb(128/255, 128/255, 128/255, 0.1),
+     ylim = c(-max_val, max_val),
+     xlim = c(-max_val, max_val),
+     cex = 0.4
+)
+abline(a = 0, b = 1, col = "gray")
+abline(h = 0, v = 0, col = "gray")
+points(y = res_hn_ribo[forwarded_hn, 2],
+       x = res_hn_rna[forwarded_hn, 2],
+       pch = 16, 
+       col = rgb(0,0,1,1),
+       cex = 0.4)
+points(y = res_hn_ribo[exclusive_hn, 2],
+       x = res_hn_rna[exclusive_hn, 2],
+       pch = 16, 
+       col = rgb(1,0,0,1),
+       cex = 0.4)
+points(y = res_hn_ribo[intensified_hn, 2],
+       x = res_hn_rna[intensified_hn, 2],
+       pch = 16, 
+       col = rgb(1,1,0,1),
+       cex = 0.4)
+points(y = res_hn_ribo[buffered_hn, 2],
+       x = res_hn_rna[buffered_hn, 2],
+       pch = 16, 
+       col = rgb(1,0,1,1),
+       cex = 0.4)
 
---
+++ 5_intersection.R
@@ -0,0 +1,108 @@
+### STEP 5: Intersect DTEGs from distinct datasets ###
+
+## This script will read the list of DTEGs and determine the overlapping genes
+## across the different data sets. Direction of changes will be preserved.
+
+# Create tables of DTEG categories created in STEP 4 via the DTEG framework
+## Exclusive DTEGs
+exclusive_tab_gscnpc <- res_gscnpc[which(res_gscnpc$padj < 0.05 &
+                                           res_gscnpc_ribo$padj < 0.05 &
+                                           res_gscnpc_rna$padj > 0.05),]
+exclusive_tab_gscnpc_pos <- exclusive_tab_gscnpc[which(exclusive_tab_gscnpc$log2FoldChange > 0),]
+exclusive_tab_gscnpc_neg <- exclusive_tab_gscnpc[which(exclusive_tab_gscnpc$log2FoldChange < 0),]
+
+
+exclusive_tab_hn <- res_hn[which(res_hn$padj < 0.05 &
+                                   res_hn_ribo$padj < 0.05 &
+                                   res_hn_rna$padj > 0.05),]
+exclusive_tab_hn_pos <- exclusive_tab_hn[which(exclusive_tab_hn$log2FoldChange > 0),]
+exclusive_tab_hn_neg <- exclusive_tab_hn[which(exclusive_tab_hn$log2FoldChange < 0),]
+
+## Intensified & Buffered DTEGs
+both_tab_gscnpc <- res_gscnpc[which(res_gscnpc$padj < 0.05 &
+                                      res_gscnpc_ribo$padj < 0.05 &
+                                      res_gscnpc_rna$padj < 0.05),]
+
+both_tab_hn <- res_hn[which(res_hn$padj < 0.05 &
+                              res_hn_ribo$padj < 0.05 &
+                              res_hn_rna$padj < 0.05),]
+
+intensified_tab_gscnpc <- both_tab_gscnpc[which(
+  res_gscnpc[rownames(both_tab_gscnpc),2]*
+    res_gscnpc_rna[rownames(both_tab_gscnpc),2] > 0),]
+
+intensified_tab_gscnpc_pos <- intensified_tab_gscnpc[which(intensified_tab_gscnpc$log2FoldChange > 0),]
+intensified_tab_gscnpc_neg <- intensified_tab_gscnpc[which(intensified_tab_gscnpc$log2FoldChange < 0),]
+
+intensified_tab_hn <- both_tab_hn[which(
+  res_hn[rownames(both_tab_hn),2]*
+    res_hn_rna[rownames(both_tab_hn),2] > 0),]
+
+intensified_tab_hn_pos <- intensified_tab_hn[which(intensified_tab_hn$log2FoldChange > 0),]
+intensified_tab_hn_neg <- intensified_tab_hn[which(intensified_tab_hn$log2FoldChange < 0),]
+
+buffered_tab_gscnpc <- both_tab_gscnpc[which(
+  res_gscnpc[rownames(both_tab_gscnpc),2]*
+    res_gscnpc_rna[rownames(both_tab_gscnpc),2] < 0),]
+
+buffered_tab_gscnpc <- rbind(
+  res_gscnpc[which(res_gscnpc$padj < 0.05 &
+                     res_gscnpc_ribo$padj > 0.05 &
+                     res_gscnpc_rna$padj < 0.05),],
+  buffered_tab_gscnpc
+)
+
+buffered_tab_gscnpc_pos <- buffered_tab_gscnpc[which(buffered_tab_gscnpc$log2FoldChange > 0),]
+buffered_tab_gscnpc_neg <- buffered_tab_gscnpc[which(buffered_tab_gscnpc$log2FoldChange < 0),]
+
+buffered_tab_hn <- both_tab_hn[which(
+  res_hn[rownames(both_tab_hn),2]*
+    res_hn_rna[rownames(both_tab_hn),2] < 0),]
+
+buffered_tab_hn <- rbind(
+  res_hn[which(res_hn$padj < 0.05 &
+                 res_hn_ribo$padj > 0.05 &
+                 res_hn_rna$padj < 0.05),],
+  buffered_tab_hn)
+
+buffered_tab_hn_pos <- buffered_tab_hn[which(buffered_tab_hn$log2FoldChange > 0),]
+buffered_tab_hn_neg <- buffered_tab_hn[which(buffered_tab_hn$log2FoldChange < 0),]
+
+# Merge tables from different DTEG categories
+tab_gscnpc <- rbind(exclusive_tab_gscnpc,
+                    intensified_tab_gscnpc,
+                    buffered_tab_gscnpc)
+tab_gscnpc_pos <- tab_gscnpc[which(tab_gscnpc$log2FoldChange > 0), ]
+tab_gscnpc_neg <- tab_gscnpc[which(tab_gscnpc$log2FoldChange < 0), ]
+
+tab_hn <- rbind(exclusive_tab_hn,
+                intensified_tab_hn,
+                buffered_tab_hn)
+tab_hn_pos <- tab_hn[which(tab_hn$log2FoldChange > 0), ]
+tab_hn_neg <- tab_hn[which(tab_hn$log2FoldChange < 0), ]
+
+# Visualize overlap with Venn diagram
+## Intersect the DTEGs identified from the 'hypoxia versus normoxia' contrast with
+## DTGs (RNA-seq only) identified from the 'GSC versus NPC' contrast
+require(VennDiagram)
+A <- read.table(file = "results/DEGs_gscnpc.txt", header = TRUE, row.names = 1) %>%
+  filter(log2FoldChange > 0) %>% rownames()
+B <- rownames(tab_hn_pos)
+overlapAB <- calculate.overlap(
+  x = list("GSC v. NPC DEGs"= A, "Hypoxia v. Normoxia DTEGs"= B))
+
+venn.plot <- draw.pairwise.venn(
+  area1 = length(overlapAB$a1),
+  area2 = length(overlapAB$a2),
+  cross.area = length(overlapAB$a3),
+  category = c("GSC v. NPC\nUpregulated DEGs in GSCs", "Hypoxia v. Normoxia\n Upregulated DTEGs in Hypoxic GSCs"), 
+  fill = c("blue", "red"),
+  lty = "blank",
+  cex = 2,
+  cat.cex = 1,
+  cat.pos = c(180, 180),
+  cat.dist = 0.05,
+  cat.just = list(c(0, 1), c(1, 1))
+)
+grid.draw(venn.plot)
+
